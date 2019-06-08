bc3978f1a7c0b6a5e646f79c69037d65d7460e0f
Alan Maldonado <business.alanm@gmail.com>
2019-06-08 18:41:30 -0500

    wrong-push

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import SeasonDisplay from './components/SeasonDisplay/SeasonDisplay';
 
 const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    position => console.log(position),
+    failure => console.log(failure),
+  );
+
   return (
     <React.Fragment>
       <div />
