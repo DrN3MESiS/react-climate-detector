aeeb75d15ba55001889e2f962336c7fbda11ec63
Alan Maldonado <business.alanm@gmail.com>
2019-06-08 20:22:22 -0500

    starting-state-comp

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import SeasonDisplay from './components/SeasonDisplay/SeasonDisplay';
+//import SeasonDisplay from './components/SeasonDisplay/SeasonDisplay';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => console.log(position),
-    failure => console.log(failure),
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = [];
+  }
 
-  return (
-    <React.Fragment>
-      <div />
-    </React.Fragment>
-  );
-};
+  //Basic equirement
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position),
+      failure => console.log(failure),
+    );
+
+    return (
+      <React.Fragment>
+        <div>
+          <h1>Loading...</h1>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
 
 export default App;
