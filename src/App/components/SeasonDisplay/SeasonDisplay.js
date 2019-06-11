4fc3abea1ccbdfe42fd28081d24ebb44c6fce0d2
Alan Maldonado <business.alanm@gmail.com>
2019-06-11 14:26:51 -0500

    added-custom-css-lib

--- src/App/components/SeasonDisplay/SeasonDisplay.js
+++ src/App/components/SeasonDisplay/SeasonDisplay.js
@@ -1,7 +1,42 @@
 import React from 'react';
 
+const seasonConfig = {
+  summer: {
+    text: "It's hot as fuck!",
+    iconName: 'sun',
+    color: 'orange',
+  },
+  winter: {
+    text: "It's fucking cold!",
+    iconName: 'snowflake',
+    color: 'blue',
+  },
+};
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+};
+
 const SeasonDisplay = props => {
-  return <div>{props.lat}</div>;
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { text, iconName, color } = seasonConfig[season];
+  return (
+    <div align="CENTER" style={{ paddingTop: 1 + 'rem' }}>
+      <i
+        aria-hidden="true"
+        className={`${iconName} loading icon massive ${color}`}
+      />
+      <h1>{text}</h1>
+      <i
+        aria-hidden="true"
+        className={`${iconName} loading icon massive ${color}`}
+      />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
