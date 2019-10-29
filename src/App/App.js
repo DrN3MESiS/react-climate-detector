152fa813c1a1f12edad0a71ea01cb5fd92a82a73
Alan Maldonado <business.alanm@gmail.com>
2019-10-28 23:05:15 -0600

    UPDATE: Component refactoring

--- src/App/App.js
+++ src/App/App.js
@@ -1,56 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SeasonDisplay from './components/SeasonDisplay/SeasonDisplay';
 import Loader from './components/Loader/Loader';
 
-class App extends React.Component {
-  state = { lat: null, errMsg: '' };
-
-  componentDidMount() {
+const App = () => {
+  const [lat, setLat] = useState(null);
+  const [err, setErr] = useState('');
+  useEffect(() => {
     console.log('Getting user location...');
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({ lat: position.coords.latitude });
-        console.log('Success');
-      },
-      err => {
-        this.setState({
-          errMsg: err.message,
-        });
-        console.log('Error, user did not accept the request');
-      },
+      position => setLat(position.coords.latitude),
+      err => setErr(err.message),
+    );
+  }, []);
+
+  let content;
+
+  if (err && !lat) {
+    content = (
+      <React.Fragment>
+        <div>Error: {err}</div>
+      </React.Fragment>
+    );
+  } else if (lat && !err) {
+    content = (
+      <React.Fragment>
+        <SeasonDisplay lat={lat} />
+      </React.Fragment>
+    );
+  } else if (!err && !lat) {
+    content = (
+      <React.Fragment>
+        <Loader content="Loading your location... (Please allow us to use your location by accepting the request)" />
+      </React.Fragment>
     );
   }
 
-  componentDidUpdate() {
-    console.log('Data has been updated...');
-  }
-
-  renderContent() {
-    if (this.state.errMsg && !this.state.lat) {
-      return (
-        <React.Fragment>
-          <div>Error: {this.state.errMsg}</div>
-        </React.Fragment>
-      );
-    } else if (this.state.lat && !this.state.errMsg) {
-      return (
-        <React.Fragment>
-          <SeasonDisplay lat={this.state.lat} />
-        </React.Fragment>
-      );
-    } else if (!this.state.errMsg && !this.state.lat) {
-      return (
-        <React.Fragment>
-          <Loader content="Loading your location... (Please allow us to use your location by accepting the request)" />
-        </React.Fragment>
-      );
-    }
-  }
-
-  //Basic equirement
-  render() {
-    return <div>{this.renderContent()}</div>;
-  }
-}
+  return content;
+};
 
 export default App;
